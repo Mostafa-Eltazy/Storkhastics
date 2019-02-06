1321829e2390bfe90576ef697f811c3e7f0488c2
MostafaElTazy <mweltazy@gmail.com>
2019-02-06 03:50:42 +0200

    intial enviroment

---
+++ Storkhastics-Game/Assets/Scripts/thirdPersonCameraWithRotation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdPersonCameraWithRotation : MonoBehaviour {
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 30.0f;
+
+    public Transform lookat;
+    public Transform camertransform;
+
+    public float rotatespeed;
+
+    public float distance = 5.0f;
+    public float currentX = 0.0f;
+    public float currentY = 0.0f;
+    public float SenstivityX = 4.01f;
+    public float SentivityY = 1.01f;
+
+
+    void Start()
+    {
+
+        camertransform = transform;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentY += Input.GetAxis("Mouse X");
+        currentX += Input.GetAxis("Mouse Y");
+        currentX = Mathf.Clamp(currentX, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        /////////////////////////////////////////////////
+        float horizontal = Input.GetAxis("Mouse X") * rotatespeed;
+        lookat.transform.Rotate(0, horizontal, 0);
+        float desiredAngle = lookat.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(currentX, currentY, 0);
+        /////////////////////////////////////////////////
+        camertransform.position = lookat.position + rotation * dir;
+        camertransform.LookAt(lookat.position);
+    }
+}
---
+++ Storkhastics-Game/Assets/Scripts/walking.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walking : MonoBehaviour {
+
+    public float movespeed;
+    public float jumpheight;
+    public Rigidbody body;
+    ///////////////////////
+    private bool grounded;
+    public Collider groundcheck;
+    //////////////////////
+    public float smooth;
+    private Quaternion targetrotation;
+	// Use this for initialization
+	void Start () {
+		
+        body=GetComponent<Rigidbody>();
+        groundcheck = GetComponent<Collider>();
+        targetrotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //to chech if grounded
+        grounded = Physics.Raycast(transform.position, Vector3.down, groundcheck.bounds.extents.y + 0.1f);
+        //to move forward
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            float vertical =  movespeed * Time.deltaTime;
+            transform.Translate(0, 0, vertical);
+        }
+        /////////////  Auto set up ///////////////////
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            targetrotation *= Quaternion.AngleAxis(45, Vector3.down);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            targetrotation *= Quaternion.AngleAxis(45, Vector3.up);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && grounded)
+        {
+            targetrotation *= Quaternion.AngleAxis(90, Vector3.down);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetrotation, 10 * smooth * Time.deltaTime); 
+    //////////To rotate around it's own axis //
+    //////////   Manual setup  ///////////////
+    //    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    //    {
+    //        transform.Rotate(0,-45.0f, 0);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.RightArrow))
+    //    {
+    //         transform.Rotate(0,45.0f, 0);
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.DownArrow))
+    //     {
+    //         transform.Rotate(0,180.0f, 0);
+    //     }
+	}
+}
