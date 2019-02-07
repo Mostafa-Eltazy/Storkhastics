9f3f57f3f57bdfdbdfd85ac1f2cb52c6e36833ef
MostafaElTazy <mweltazy@gmail.com>
2019-02-07 06:35:26 +0200

    intergation

---
+++ Storkhastics-Game/Assets/Scripts/AutoFlying.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoFlying : MonoBehaviour
+{
+
+    private bool grounded;
+    public Collider groundcheck;
+    public Rigidbody body;
+    ////////////////////////////
+    public float alttitude;
+    public float glide;
+    public float gravity;
+    public float brake;
+    public GameObject directionpoint;
+    private Quaternion targetrotation;
+    /////////////////////////////
+    public bool preparetoboost;
+    private float boostingwindow;
+    public float timeforboosting;
+    public float boostingforce;
+    /////////////////////////////////
+    // Use this for initialization
+    void Start()
+    {
+        body = FindObjectOfType<Rigidbody>();
+        preparetoboost = false;
+        boostingwindow = timeforboosting;
+        targetrotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (preparetoboost)
+        {
+            boostingwindow -= Time.deltaTime;
+            if (Input.GetKeyUp(KeyCode.Space) && !grounded)
+                {
+                  if (boostingwindow > 0.0f)
+                   {
+                       Debug.Log("fffffffffffffffffffffffffffffffff");
+                     body.AddForce(transform.forward * (boostingforce), ForceMode.Force);
+                   }
+                }
+            if (boostingwindow < 0.0f) {
+                preparetoboost = false;
+            }
+        }
+            Physics.gravity = new Vector3(0, gravity, 0);
+            grounded = Physics.Raycast(transform.position, Vector3.down, groundcheck.bounds.extents.y + 0.1f);
+            if (Input.GetKey(KeyCode.Space) && !grounded)
+            {
+
+                float vertical = Input.GetAxis("Vertical") * (alttitude + 1) * Time.deltaTime;
+                transform.Translate(0, 0, vertical);
+
+            }
+            if (Input.GetKeyUp(KeyCode.Space) && !grounded)
+            {
+                Debug.Log("jidsjfs");
+                boostingwindow = timeforboosting;
+                preparetoboost = true;
+
+
+            } 
+            if (Input.GetKeyDown(KeyCode.DownArrow)&&!grounded)
+            {
+                Physics.gravity = new Vector3(0, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                body.velocity = new Vector3(body.velocity.x, alttitude, body.velocity.z);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                body.velocity = new Vector3(body.velocity.x, -glide, body.velocity.z);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow)&&!grounded)
+            {
+
+                
+                
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow)&&!grounded)
+            {
+                
+
+            }
+            if (Input.GetKey(KeyCode.C) && !grounded)
+            {
+               
+               body.AddForce(-(transform.forward * (brake)), ForceMode.Force);
+            }
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetrotation, 10 * (0.2f) * Time.deltaTime);
+        }
+    }
+
---
+++ Storkhastics-Game/Assets/Scripts/Flying.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    private bool grounded;
+    public Collider groundcheck;
+    public Rigidbody body;
+    public float groundedhoveralttitude;
+    public float airbornhoveralttitude;
+    public float WingForce;
+    public float alttitude;
+    public int boostcounter;
+    public float wingrestinterval = 2f;
+    public bool rest;
+    public bool boost;
+    public float normalgravityeffect;
+    public float lightgravityeffect;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+        Physics.gravity = new Vector3(0, normalgravityeffect, 0);
+        rest = false;
+        boost = true;
+        body = FindObjectOfType<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, groundcheck.bounds.extents.y + 0.1f);
+        if(rest){
+
+            
+            wingrestinterval-=Time.deltaTime;
+            if(wingrestinterval<0.0f){
+                boostcounter=0;
+                boost=true;
+                rest=false;
+            }
+        }
+        //To hover from ground
+        if (Input.GetButton("Jump") && grounded)
+        {
+            Debug.Log("ground hover");
+            body.AddForce(transform.up * groundedhoveralttitude);
+        }
+        //To hover while airborn
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("air hover");
+            body.AddForce(transform.up * airbornhoveralttitude);
+        }
+        //To eject his body forward
+        if (Input.GetButtonUp("Jump") && !grounded)
+        {
+            
+            Debug.Log("Fly");
+            boostcounter += 1;
+            Physics.gravity = new Vector3(0, lightgravityeffect, 0);
+            body.AddForce(transform.up * (alttitude * 0.1f), ForceMode.Force);
+         
+            if(boost)
+            {  
+                
+                if(boostcounter<2)
+                {
+                  body.AddForce(transform.up * alttitude, ForceMode.Force);
+                  body.AddForce(transform.forward * WingForce, ForceMode.Force);
+                  
+                }
+                else
+                {
+                 wingrestinterval=2.0f;
+                 rest=true;
+                 boost=false;
+                }
+            }             
+         }
+          //  transform.Translate(0, 0, 3.0f);
+            
+        }
+ 
+    }
+
